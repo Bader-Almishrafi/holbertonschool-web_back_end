75b48397445b5c0ed03d3267047741d6f83298c7
Bader-Almishrafi <baderalmishrafi@gmail.com>
2026-04-27 17:55:26 +0300

    Add index_range function and pagination README

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+def index_range(a, b):
+	"""returns a tuple of size two containing a start index and an end index
+	corresponding to the range of indexes to return in a list for those
+	particular pagination parameters.
+	"""
+	start_index = (a - 1) * b
+	end_index = a * b
+	return (start_index, end_index)
