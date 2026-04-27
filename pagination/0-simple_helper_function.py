8aa84355f1c257b520d6c765704930cfd4753eea
Bader-Almishrafi <baderalmishrafi@gmail.com>
2026-04-27 18:13:49 +0300

    change names

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-def index_range(a, b):
+def index_range(page: int, page_size: int) -> tuple:
 	"""returns a tuple of size two containing a start index and an end index
 	corresponding to the range of indexes to return in a list for those
 	particular pagination parameters.
 	"""
-	start_index = (a - 1) * b
-	end_index = a * b
+	start_index = (page - 1) * page_size
+	end_index = page * page_size
 	return (start_index, end_index)
