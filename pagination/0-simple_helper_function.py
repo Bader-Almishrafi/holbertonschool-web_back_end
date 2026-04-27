0f40b928aa9ccaea3cb1e58369f5db3df735e3b2
Bader-Almishrafi <baderalmishrafi@gmail.com>
2026-04-27 18:12:40 +0300

    add #!/usr/bin/env python3

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 def index_range(a, b):
 	"""returns a tuple of size two containing a start index and an end index
 	corresponding to the range of indexes to return in a list for those
